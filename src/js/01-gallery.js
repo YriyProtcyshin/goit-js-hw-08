c189165e1f620237e27415723fa7539fb03f857b
YriyProtcyshin <yriy.protcyshin@gmail.com>
2022-09-28 21:32:32 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,36 @@
 // Add imports above this line
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
-console.log(SimpleLightbox);
+const galleryDivRef = document.querySelector('.gallery');
 
-console.log(galleryItems);
+// 1. Создание и рендер разметки
+creatingAndRenderingMarkup(galleryItems, galleryDivRef);
+
+const options = {
+  captionsData: 'alt',
+  captionDelay: 250,
+};
+
+const lightbox = new SimpleLightbox('.gallery a', options);
+
+// ==========================================================================================
+//                                   function declaration
+// ===========================================================================================
+
+function creatingAndRenderingMarkup(arrItems, galleryDivRef) {
+  const divImageList = arrItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    `;
+    })
+    .join('');
+
+  galleryDivRef.innerHTML = divImageList;
+}
